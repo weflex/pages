845fda3cd5c9fd06b899603376353184b6b0e2e9
yorkie <yorkiefixer@gmail.com>
2015-09-11 17:21:29 +0800

    add day

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
 	$('div.counter').countdown(finalDate)
   .on('update.countdown', function(event) {
    	$(this).html(event.strftime(
+      '<div class="days-wrapper"><span class="days">%D</span><br>days</div>' + 
 	 	  '<div class="hours-wrapper"><span class="hours">%H</span><br>hours</div>' + 
 	 	  '<div class="minutes-wrapper"><span class="minutes">%M</span><br>minutes</div>' +
 	 	  '<div class="seconds-wrapper"><span class="seconds">%S</span><br>seconds</div>'
